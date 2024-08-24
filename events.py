22aef32600fa2b19633b38a929dfe52993f661a7
JoshLohner <lohnerjosh@gmail.com>
2024-08-23 21:31:25 -0400

    Basic Logo Stuff works

--- events.py
+++ events.py
@@ -7,10 +7,24 @@ class EventHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        # Find the general channel or use a specific channel by ID
         channel = discord.utils.get(member.guild.text_channels, name='general')
+        specific_channel = member.guild.get_channel(762144356156964865)
+        
         if channel:
-            total_members = member.guild.member_count
-            await channel.send(f"""Welcome to Genesis, {member.mention}! If you are interested in joining the guild, fill out the application: https://forms.gle/9PVuV6p9PvZYwtUMA \nChannels and role selection are above in PLACEHOLDER. If you are not interested in joining the guild for now, select the Friend of the Guild role or the Guild Ambassador role if you are in another guild! We are now at {total_members} members!""")
+            # Send a welcome message with an image
+            
+            await channel.send(
+                f"""Welcome to Genesis, {member.mention}! If you are interested in joining the guild, fill out the application: https://forms.gle/9PVuV6p9PvZYwtUMA \nChannels and role selection are above in {specific_channel.mention}. If you are not interested in joining the guild for now, select the Friend of the Guild role or the Guild Ambassador role if you are in another guild! We are now at {member.guild.member_count} members!"""
+            )
+
+            #Send Logo
+            await channel.send(file=discord.File('assets/new_gen_logo.png'))
+            # Get the member's profile picture
+            avatar_url = member.avatar.url if member.avatar else member.default_avatar.url
+            # Send the member's profile picture
+            await channel.send(avatar_url)
+
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
